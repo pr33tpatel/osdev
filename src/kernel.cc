cd60c2280a3f818b534906088f80dbc7e1b19783
Preet Patel <preetpatel1223@gmail.com>
2026-01-19 19:28:17 -0600

    changed hello world message

--- src/kernel.cc
+++ src/kernel.cc
@@ -114,7 +114,7 @@ extern "C" void callConstructors(){
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
     // clearScreen();
-    printf("Hello World! --- http://www.AlgorithMan.de\n");
+    printf("Hello World! :)                                                           \n");
 
     GlobalDescriptorTable gdt;
     InterruptManager interrupts(0x20, &gdt);
