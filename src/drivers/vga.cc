1d5583839843059745f15515046ace3e6dd4d979
Preet Patel <preetpatel1223@gmail.com>
2026-01-22 19:39:52 -0600

    expanded colors | added red, green, blue, white, and black

--- src/drivers/vga.cc
+++ src/drivers/vga.cc
@@ -128,10 +128,13 @@ uint8_t* VideoGraphicsArray::GetFrameBufferSegment(){
 }
 
 uint8_t VideoGraphicsArray::GetColorIndex(uint8_t r, uint8_t g, uint8_t b) {
-  if (r == 0x00 && g == 0x00 && b == 0xA8) { return 0x01; }
-  else {
-    return 0;
-  }
+  if (r == 0x00 && g == 0x00 && b == 0x00)        return 0x00; // black
+  if (r == 0x00 && g == 0x00 && b == 0xA8)        return 0x01; // blue 
+  if (r == 0x00 && g == 0xA8 && b == 0x00)        return 0x02; // green 
+  if (r == 0xA8 && g == 0x00 && b == 0x00)        return 0x04; // red 
+  if (r == 0xFF && g == 0xFF && b == 0xFF)        return 0x3F; // white  (0x3F == 63, the 64th color (2^6))
+  else                                            return 0x00; // black by default
+  
 } 
 
 void VideoGraphicsArray::PutPixel(uint32_t x, uint32_t y, uint8_t colorIndex) {
