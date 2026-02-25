17bac625e345ffbba4ada25eca5457b3a188bc9c
Preet Patel <preetpatel1223@gmail.com>
2026-02-24 19:20:01 -0600

    fix(ds): fixed namespace error in `pair.h`

--- include/utils/ds/pair.h
+++ include/utils/ds/pair.h
@@ -1,10 +1,16 @@
 #ifndef __OS__UTILS__DS__PAIR_H
 #define __OS__UTILS__DS__PAIR_H
 
+namespace os {
+namespace utils {
+namespace ds {
 template <typename K, typename V>
 struct Pair {
   K key;
   V value;
 };
+}  // namespace ds
+}  // namespace utils
+}  // namespace os
 
 #endif
